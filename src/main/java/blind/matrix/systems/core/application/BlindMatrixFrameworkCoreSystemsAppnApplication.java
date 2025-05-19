7fc2524ef8f6755dccd3dc883a05fe955c3adab4
Ritesh RightShip <ritesh.chawla@blindmatrix.digital>
2025-05-19 10:24:58 +0530

    initial

---
+++ src/main/java/blind/matrix/systems/core/application/BlindMatrixFrameworkCoreSystemsAppnApplication.java
@@ -0,0 +1,13 @@
+package blind.matrix.systems.core.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindMatrixFrameworkCoreSystemsAppnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlindMatrixFrameworkCoreSystemsAppnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blind/matrix/systems/core/application/BlindMatrixFrameworkCoreSystemsAppnApplicationTests.java
@@ -0,0 +1,13 @@
+package blind.matrix.systems.core.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindMatrixFrameworkCoreSystemsAppnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
